b80629720b9aabe23e736acb050fd1de6c1f38db
Arindamghosh01 <arindamghosh7878@gmail.com>
2021-09-16 17:35:09 +0530

    Few changes in main

--- main.cpp
+++ main.cpp
@@ -6,14 +6,13 @@
 #include <vector>
 #include <set>
 
-#define BOARD_ROWS 3
-#define BOARD_COLS 3
+#define BOARD_SIZE 3
 
-class Game {
+class Game{
 private:
     bool isEnd;
     int player_turn;
-    std::array<std::array<int, BOARD_COLS>, BOARD_ROWS> board;
+    std::array<std::array<int, BOARD_SIZE>, BOARD_SIZE> board;
 
     // Reset the Board
     void reset () {
@@ -27,8 +26,8 @@ private:
     std::set<std::pair<int, int>> available_positions() {
         std::set<std::pair<int, int>> a;
 
-        for (int i = 0; i < BOARD_ROWS; i++) {
-            for (int j = 0; j < BOARD_COLS; j++) {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
                 if (board[i][j] == 0)
                     a.insert({i, j});
             }
@@ -40,45 +39,47 @@ private:
     // Check for Winner
     int check_winner() {
         // For Rows
-        for (int i = 0; i < BOARD_COLS; i++) {
+        for (int i = 0; i < BOARD_SIZE; i++) {
             int sum = 0;
-            for (int j = 0; j < BOARD_ROWS; j++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
                 sum += board[i][j];
             }
 
-            if (abs(sum) == 3) {
+            if (abs(sum) == BOARD_SIZE) {
                 isEnd = true;
-                return sum/3;
+                return sum/BOARD_SIZE;
             }
         }
 
         // For column
-        for (int i = 0; i < BOARD_ROWS; i++) {
+        for (int i = 0; i < BOARD_SIZE; i++) {
             int sum = 0;
-            for (int j = 0; j < BOARD_COLS; j++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
                 sum += board[j][i];
             }
 
-            if (abs(sum) == 3) {
+            if (abs(sum) == BOARD_SIZE) {
                 isEnd = true;
-                return sum/3;
+                return sum/BOARD_SIZE;
             }
         }
 
         // For Diagonal
         int dia1 = 0, dia2 = 0;
-        for (int i = 0; i < BOARD_ROWS; i++) {
-            for (int j = 0; j < BOARD_COLS; j++) {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
                 if (i == j)
                     dia1 += board[i][j];
-                if (i == BOARD_COLS - j - 1)
-                    dia2 += board[i][BOARD_COLS - i - 1];
+                if (i == BOARD_SIZE - j - 1)
+                    dia2 += board[i][BOARD_SIZE - i - 1];
             }
         }
 
-        if (abs(dia1) == 3 || abs(dia2) == 3) {
+        if (abs(dia1) == BOARD_SIZE || abs(dia2) == BOARD_SIZE) {
             isEnd = true;
-            return abs(dia1) == 3 ? dia1/3 : dia2/3;
+            return abs(dia1) == BOARD_SIZE ?
+                    dia1 / BOARD_SIZE :
+                    dia2 / BOARD_SIZE;
         }
 
         // For Checking Draw Games
@@ -92,11 +93,12 @@ private:
 
     // Input Pattern for Human understanding :)
     void show_input_pattern() {
-        for (int i = 0; i < BOARD_ROWS; i++) {
+        std::cout << "Choose the position below ...." << '\n';
+        for (int i = 0; i < BOARD_SIZE; i++) {
             std::cout << "-------------" << '\n';
             std::cout << '|';
-            for (int j = 0; j < BOARD_COLS; j++) {
-                std::cout <<  ' ' << (BOARD_ROWS * i + j) << ' ';
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                std::cout <<  ' ' << (BOARD_SIZE * i + j) << ' ';
                 std::cout << '|';
             }
             std::cout << '\n';
@@ -107,10 +109,10 @@ private:
 
     // Display the board
     void show_board () {
-        for (int i = 0; i < BOARD_ROWS; i++) {
+        for (int i = 0; i < BOARD_SIZE; i++) {
             std::cout << "-------------" << '\n';
             std::cout << '|';
-            for (int j = 0; j < BOARD_COLS; j++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
                 if (board[i][j] == 1) {
                     std::cout << " X ";
                 }
@@ -134,11 +136,11 @@ private:
             std::cout << "Input your Action Position 0-8: ";
             std::cin >> x;
 
-            std::pair action = std::make_pair(x / 3, x % 3);
+            std::pair action = std::make_pair(x / BOARD_SIZE, x % BOARD_SIZE);
             if (pos.find(action) != pos.end()) {
                 return action;
             } else {
-                std::cout << "Position out of bound OR place is Preoccupied."
+                std::cout << "Position out of bound OR is Preoccupied."
                      << " Try again...\n";
             }
         }
@@ -150,6 +152,9 @@ private:
         player_turn = player_turn == 1 ? -1 : 1;
     }
 
+    void minimax() {
+        
+    }
 
 public:
     Game() {
@@ -158,6 +163,14 @@ public:
 
     // Game Between Two Humans
     void play() {
+        std::cout
+<< "             _______________" << '\n'
+<< "            |  ___________  |" << '\n'
+<< "            | | TICTACTOE | |" << '\n'
+<< "            | |___________| |" << '\n'
+<< "            |_______________|" << "\n\n";
+
+
         show_input_pattern();
 
         // Game Loop
@@ -179,13 +192,49 @@ public:
             }
         }
     }
+
+    // Play against computer
+    void play_against_ai() {
+        show_input_pattern();
+
+        // Game Loop
+        while (1) {
+            // You'll go first, human should have the advantage coz let's face
+            // it computer can do computation fast :(
+
+            std::set<std::pair<int, int>> pos = available_positions();
+            std::pair<int, int> action;
+
+            if (player_turn == 1) {
+                action = choose_action (pos);
+            }
+            else {
+                minimax();
+                action = choose_action (pos);
+            }
+
+            switch_player(action);
+            show_board();
+
+            int win = check_winner();
+            if (isEnd) {
+                if (win == 1)
+                    std::cout << "Let's go Team Human ^_^!\n";
+                else if (win == -1)
+                    std::cout << "Computer Wins!\n";
+                else
+                    std::cout << "The Game ends in a tie! Not bad.\n";
+                break;
+            }
+        }
+    }
 };
 
 
 int main() {
-    Game g;
+    Game tictactoe;
 
-    g.play();
+    tictactoe.play();
 
     return 0;
 }
